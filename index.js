df57b2977f370f5327c752d6225d99cc8dff6768
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2023-10-16 14:16:55 +0700

    v1.1.0

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import config from './config.js';
+import fs from 'fs';
 import axios from 'axios';
+import cron from 'node-cron';
+import { JSDOM } from 'jsdom';
+import morgan from 'morgan';
+import config from './config.js';
+
+const packageJSON = JSON.parse(
+	fs.readFileSync('./package.json', 'utf-8').toString(),
+);
+
 const oembed = (provider_name, provider_url, author_name, author_url, url) => {
 	const baseObject = {
 		version: '1.0',
@@ -16,153 +25,203 @@ const oembed = (provider_name, provider_url, author_name, author_url, url) => {
 	if (author_url) {
 		baseObject.author_url = author_url;
 	}
-    if (url) {
-        baseObject.url = url;
-    }
+	if (url) {
+		baseObject.url = url;
+	}
 	return baseObject;
 };
 
 // Main
 const app = express();
 
-const cache = new Map() // Collection<path,full>
-
 app.use(bodyParser.json());
+
 app.use(
 	bodyParser.urlencoded({
 		extended: true,
 	}),
 );
+app.use(morgan('dev'));
 
-const randomURLPath = (length = 6) => {
-	const allChar =
-		'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	let result = '';
-	for (let i = 0; i < length; i++) {
-		result += allChar.charAt(Math.floor(Math.random() * allChar.length));
-	}
-	return result;
-};
+app.use((req, res, next) => {
+	res.header('Cache-Control', 'no-cache, no-store, must-revalidate'); // HTTP 1.1.
+	res.header('Pragma', 'no-cache'); // HTTP 1.0.
+	res.header('Expires', '0'); // Proxies.
+	next();
+});
 
 // embed
-app.get('/', (req, res) => res.status(200).send('hello world'));
+app.get('/', (req, res) =>
+	res
+		.status(200)
+		.send(
+			`WebEmbed v${packageJSON.version} by Elysia<br>Github: https://github.com/aiko-chan-ai/WebEmbed`,
+		),
+);
 
 app.get('/embed', (req, res) => {
-    if (req.query.image_type && !['image', 'thumbnail'].includes(req.query.image_type)) return res.status(400).send('Invalid image type specified\nType: "image" or "thumbnail"');
-    const {
-        provider_name,
-        provider_url,
-        author_name,
-        author_url,
-        image_type = 'thumbnail',
-        image,
-        video,
-        color,
-        url,
-        description,
-        title,
-        redirect,
-    } = req.query;
-    let html = '<html>';
-    html += '\n<head>';
-    // join oembed
-    let oembedURL = (process.env.WEBURL || config.WebURL) + 'oembed?';
-    if (provider_name) {
-        oembedURL += `provider_name=${encodeURIComponent(provider_name)}&`;
-    }
-    if (provider_url) {
-        oembedURL += `provider_url=${encodeURIComponent(provider_url)}&`;
-    }
-    if (author_name) {
-        oembedURL += `author_name=${encodeURIComponent(author_name)}&`;
-    }
-    if (author_url) {
-        oembedURL += `author_url=${encodeURIComponent(author_url)}&`;
-    }
-    if (url) {
-        oembedURL += `url=${encodeURIComponent(url)}&`;
-    }
-    oembedURL = oembedURL.slice(0, -1);
-    // add oembed to html
-    html += `\n<link type="application/json+oembed" href="${oembedURL}" />`;
-    if (image_type == 'image') {
-        html += '\n<meta name="twitter:card" content="summary_large_image">';
-    }
-    if (image) {
-        html += `\n<meta property="og:image" content="${image}">`;
-    }
-    if (color) {
-        html += `\n<meta name="theme-color" content="${color}">`;
-    }
-    if (description) {
-        html += `\n<meta property="og:description" content="${description}">`;
-    }
-    if (provider_name && !provider_url) {
-        html += `\n<meta property="og:site_name" content="${provider_name}">`;
-    }
-    if (title) {
-        html += `\n<meta content="${title}" property="og:title">
-<meta property="twitter:title" content="${title}">`;
-    }
-    if (url) {
-        html += `\n<meta content="${url}" property="og:url">
-<meta property="twitter:url" content="${url}">\n
-<meta name="url" content="${url}">`;
-    }
-    if (video) {
-        html += `
-<meta property="og:type" content="video.other">
-<meta property="og:video" content="${video}">
-<meta property="og:video:secure_url" content="${video}">
-<meta property="og:video:width" content="1280">
-<meta property="og:video:width" content="720" />
-<meta property="og:video:type" content="text/html">`;
-    }
-    if (redirect) {
-        html += `
-<meta http-equiv="refresh" content="0; url=${redirect}" />`;
-    }
-    html += `
-</head>
-<body>
-<p>Copy <a href="${process.env.WEBURL || config.WebURL}${req.originalUrl.slice(1)}">this URL</a> to Discord Message</p>
-</body>
-</html>`;
-    return res.status(200).send(html);
+	const dom = new JSDOM();
+	const document = dom.window.document;
+	if (
+		req.query.image_type &&
+		!['image', 'thumbnail'].includes(req.query.image_type)
+	)
+		return res
+			.status(400)
+			.send('Invalid image type specified\nType: "image" or "thumbnail"');
+	const {
+		provider_name,
+		provider_url,
+		author_name,
+		author_url,
+		image_type = 'thumbnail',
+		image,
+		video,
+		color,
+		url,
+		description,
+		title,
+		redirect,
+	} = req.query;
+	if (provider_name || provider_url || author_name || author_url || url) {
+		const query = new URLSearchParams();
+		if (provider_name) {
+			query.set('provider_name', provider_name);
+		}
+		if (provider_url) {
+			query.set('provider_url', provider_url);
+		}
+		if (author_name) {
+			query.set('author_name', author_name);
+		}
+		if (author_url) {
+			query.set('author_url', author_url);
+		}
+		if (url) {
+			query.set('url', url);
+		}
+		const oembedURL = './oembed?' + query.toString();
+		const oEmbed = document.createElement('link');
+		oEmbed.setAttribute('type', 'application/json+oembed');
+		oEmbed.setAttribute('href', oembedURL);
+		document.head.appendChild(oEmbed);
+	}
+	if (image_type == 'image') {
+		const metaElement = document.createElement('meta');
+		metaElement.setAttribute('name', 'twitter:card');
+		metaElement.setAttribute('content', 'summary_large_image');
+		document.head.appendChild(metaElement);
+	}
+	if (image) {
+		const metaElement = document.createElement('meta');
+		metaElement.setAttribute('property', 'og:image');
+		metaElement.setAttribute('content', image);
+		document.head.appendChild(metaElement);
+	}
+	if (color) {
+		const metaElement = document.createElement('meta');
+		metaElement.setAttribute('name', 'twitter:card');
+		metaElement.setAttribute('content', 'summary_large_image');
+		document.head.appendChild(metaElement);
+	}
+	if (description) {
+		const metaElement = document.createElement('meta');
+		metaElement.setAttribute('property', 'og:description');
+		metaElement.setAttribute('content', description);
+		document.head.appendChild(metaElement);
+	}
+	if (provider_name && !provider_url) {
+		const metaElement = document.createElement('meta');
+		metaElement.setAttribute('property', 'og:site_name');
+		metaElement.setAttribute('content', provider_name);
+		document.head.appendChild(metaElement);
+	}
+	if (title) {
+		const metaElement1 = document.createElement('meta');
+		metaElement1.setAttribute('property', 'og:title');
+		metaElement1.setAttribute('content', title);
+		document.head.appendChild(metaElement1);
+		const metaElement2 = document.createElement('meta');
+		metaElement2.setAttribute('property', 'twitter:title');
+		metaElement2.setAttribute('content', title);
+		document.head.appendChild(metaElement2);
+	}
+	if (url) {
+		const metaElement1 = document.createElement('meta');
+		metaElement1.setAttribute('property', 'og:url');
+		metaElement1.setAttribute('content', url);
+		document.head.appendChild(metaElement1);
+		const metaElement2 = document.createElement('meta');
+		metaElement2.setAttribute('property', 'twitter:url');
+		metaElement2.setAttribute('content', url);
+		document.head.appendChild(metaElement2);
+		const metaElement3 = document.createElement('meta');
+		metaElement3.setAttribute('name', 'url');
+		metaElement3.setAttribute('content', url);
+		document.head.appendChild(metaElement3);
+	}
+	if (video) {
+		const type_meta = document.createElement('meta');
+		type_meta.setAttribute('property', 'og:type');
+		type_meta.setAttribute('content', 'video.other');
+		const video_meta = document.createElement('meta');
+		video_meta.setAttribute('property', 'og:video');
+		video_meta.setAttribute('content', video);
+		const video_secure_url = document.createElement('meta');
+		video_secure_url.setAttribute('property', 'og:video:secure_url');
+		video_secure_url.setAttribute('content', video);
+		const video_width = document.createElement('meta');
+		video_width.setAttribute('property', 'og:video:width');
+		video_width.setAttribute('content', '1280');
+		const video_height = document.createElement('meta');
+		video_height.setAttribute('property', 'og:video:height');
+		video_height.setAttribute('content', '720');
+		const video_type = document.createElement('meta');
+		video_type.setAttribute('property', 'og:video:type');
+		video_type.setAttribute('content', 'text/html');
+		document.head.appendChild(type_meta);
+		document.head.appendChild(video_meta);
+		document.head.appendChild(video_secure_url);
+		document.head.appendChild(video_height);
+		document.head.appendChild(video_width);
+		document.head.appendChild(video_type);
+	}
+	if (redirect) {
+		const redirectMeta = document.createElement('meta');
+		redirectMeta.setAttribute('http-equiv', 'refresh');
+		redirectMeta.setAttribute('content', `0; url=${redirect}`);
+		document.head.appendChild(redirectMeta);
+	}
+	document.body.innerHTML = `<p>Copy <a href="" id="url">this URL</a> to Discord Message</p><p>Github: https://github.com/aiko-chan-ai/WebEmbed</p>`;
+	const script = document.createElement('script');
+	script.textContent = "(function () { const a = document.getElementById('url'); a.setAttribute('href', window.location.href) })();";
+	document.body.appendChild(script);
+	return res.status(200).send(dom.serialize());
 });
 // oembed metadata
 app.get('/oembed', (req, res) => {
-    const {
-        provider_name,
-        provider_url,
-        author_name,
-        author_url,
-        url,
-    } = req.query;
-    return res.status(200).send(oembed(provider_name, provider_url, author_name, author_url, url));
+	const { provider_name, provider_url, author_name, author_url, url } =
+		req.query;
+	return res
+		.status(200)
+		.send(
+			oembed(provider_name, provider_url, author_name, author_url, url),
+		);
 });
-app.get('/short', function(req, res) {
-    const url = req.query.url;
-    if (!url) {return res.status(400).send({ msg: 'error', code: 400 });}
-    const randomURL = cache.findKey((v) => v == url) || randomURLPath(8);
-    cache.set(randomURL, url);
-    return res.status(200).send(`${process.env.WEBURL || config.WebURL}short/${randomURL}`);
+
+app.use(function (req, res) {
+	res.status(404).send({
+		msg: 'URL not found',
+		code: 404,
+		data: req.originalUrl + ' is incorrect',
+	});
 });
-app.get('/short/:path', async (req, res) => {
-    const path = req.params.path;
-    const data = cache.get(path);
-    if (!data) {return res.status(404).send({ msg: 'error URL', code: 404 });}
-    return res.redirect(data);
-});
-app.use(function(req, res) {
-    res.status(404).send({
-        msg: 'URL not found',
-        code: 404,
-        data: req.originalUrl + ' is incorrect',
-    });
-});
-//
+
 const server = app.listen(process.env.PORT ?? config.port, (error) => {
-    if (error) return console.log(`Error: ${error}`);
-    console.log(`PORT ${server.address().port}`);
+	if (error) return console.log(`Error: ${error}`);
+	console.log(`PORT ${server.address().port}`);
+    const task = cron.schedule('*/5 * * * *', () => {
+		axios.get(process.env.WEBURL || config.WebURL).catch(() => {});
+	});
+    task.start();
 });
